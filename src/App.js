ae0cb4ff393bba9e2de5cc72c3bcba306a5447ee
lucas.faria <lucas.faria@ambevtech.com.br>
2023-02-14 20:55:05 -0300

    added header content

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
+import Header from "./components/Layout/Header";
+
 function App() {
   return (
     <div>
-      <h2>Let's try</h2>
+      <h2>Let's try a simple app</h2>
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
     </div>
   );
 }
