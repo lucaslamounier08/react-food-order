778739075970a04fea39dc79658d713002416948
lucas.faria <lucas.faria@ambevtech.com.br>
2023-02-16 15:11:18 -0300

    removed useless test

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders lets try', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/let's try/i);
-  expect(linkElement).toBeInTheDocument();
-});
