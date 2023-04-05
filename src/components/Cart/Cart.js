99aa45a51fa5c43c9db4ab8119828e2f5f5ce329
lucas.faria <lucas.faria@ambevtech.com.br>
2023-04-05 16:15:25 -0300

    beautify char itens in modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
+
 import CartContext from "../../Store/cart-context";
-
 import Modal from "../UI/Modal";
-
+import CartItem from "./CartItem";
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
@@ -11,10 +11,21 @@ const Cart = (props) => {
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
+  const cartItemRemoveHandler = (id) => {};
+
+  const cartItemAddHandler = (item) => {};
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
